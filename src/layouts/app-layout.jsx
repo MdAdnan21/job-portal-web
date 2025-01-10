81f2c8f99bd0fdad5a6ff39f616a82db4a755562
Mohammed Adnan <jrdev1@webranc.com>
2025-01-10 11:36:16 +0530

    changes of design

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -10,8 +10,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by RoadsideCoder
-      </div>
+      Crafted with 💖 by MdAdnan </div>
     </div>
   );
 };
